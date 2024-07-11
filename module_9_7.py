87475e79cf457d383e1c65c7998462aacaa12114
Morff3 <166973084+Morff3@users.noreply.github.com>
2024-07-11 22:52:25 +0700

    Update module_9_7.py

--- module_9_7.py
+++ module_9_7.py
@@ -1,21 +1,18 @@
-# -*- coding: utf-8 -*-
 def is_prime(func):
     def wrapper(*args):
         add = func(*args)
-        for i in range(2, add):
+        count = 0
+        for i in range(1, add + 1):
             if add % i == 0:
-                return f'Составное \n{add}'
-            else:
-                return f'Простое \n{add}'
-        return add
-
+                count += 1
+        return f'Простое \n{add}' if count == 2 else f'Составное \n{add}'
     return wrapper
 
 
 @is_prime
 def sum_three(a, b, c):
-    return a + b +c
+    return a + b + c
 
 
-result = sum_three(2, 3, 6)
+result = sum_three(3, 3, 3)
 print(result)
